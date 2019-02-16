cb8ebf3645c99ae2f8445548ada10e0546754058
Evan Rupert <rupertevanr@gmail.com>
2019-02-16 14:52:00 -0500

    Added basic display of events next to the calendar

--- ui/src/app/main/workshops-list/workshops-list.component.ts
+++ ui/src/app/main/workshops-list/workshops-list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core'
 import { Event } from '../../models/event'
 import { MatDialog } from '@angular/material'
 import { WorkshopDetailsComponent } from './workshop-details/workshop-details.component'
+import { subWeeks } from 'date-fns'
 
 @Component({
   selector: 'workshops-list',
@@ -20,7 +21,45 @@ export class WorkshopsListComponent implements OnInit {
         primary: '#1e90ff',
         secondary: '#d1e8ff'
       },
-      description: 'Today we will be talking about go and elixir ...'
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse in eros est. ' +
+        'Quisque pulvinar orci ex, quis finibus lectus sagittis nec. ' +
+        'Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; ' +
+        'Quisque auctor lacinia ante nec ullamcorper. Ut viverra dui quis turpis tincidunt consectetur. ' +
+        'Nullam blandit neque eget justo maximus suscipit. Quisque malesuada in sem sit amet lobortis. ' +
+        'Donec sit amet arcu ut lacus dignissim aliquet. Sed maximus eleifend hendrerit. ' +
+        'Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.'
+    },
+    {
+      start: subWeeks(new Date(), 1),
+      end: subWeeks(new Date(), 1),
+      title: 'An old event',
+      color: {
+        primary: '#1e90ff',
+        secondary: '#d1e8ff'
+      },
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse in eros est. ' +
+        'Quisque pulvinar orci ex, quis finibus lectus sagittis nec. ' +
+        'Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; ' +
+        'Quisque auctor lacinia ante nec ullamcorper. Ut viverra dui quis turpis tincidunt consectetur. ' +
+        'Nullam blandit neque eget justo maximus suscipit. Quisque malesuada in sem sit amet lobortis. ' +
+        'Donec sit amet arcu ut lacus dignissim aliquet. Sed maximus eleifend hendrerit. ' +
+        'Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.'
+    },
+    {
+      start: subWeeks(new Date(), 2),
+      end: subWeeks(new Date(), 2),
+      title: 'Webscraping with python and beautifulsoup',
+      color: {
+        primary: '#1e90ff',
+        secondary: '#d1e8ff'
+      },
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse in eros est. ' +
+        'Quisque pulvinar orci ex, quis finibus lectus sagittis nec. ' +
+        'Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; ' +
+        'Quisque auctor lacinia ante nec ullamcorper. Ut viverra dui quis turpis tincidunt consectetur. ' +
+        'Nullam blandit neque eget justo maximus suscipit. Quisque malesuada in sem sit amet lobortis. ' +
+        'Donec sit amet arcu ut lacus dignissim aliquet. Sed maximus eleifend hendrerit. ' +
+        'Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.'
     }
   ]
 
